d6487595806b7e7510645e1f670f0a940efa24cd
romuloalvesbm <romuloalves.br@gmail.com>
2024-04-23 14:24:49 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroClientesComponent } from './cadastro-clientes/cadastro-clientes.component';
+import { ConsultaClientesComponent } from './consulta-clientes/consulta-clientes.component';
+
+//importando as classes para uso da biblioteca de rotas angular
+import { Routes, RouterModule} from '@angular/router';
+
+//Mapear uma rota (url) para cadas componente do projeto
+const routes : Routes = [
+  { path: 'cadastro-clientes', component : CadastroClientesComponent },
+  { path: 'consulta-clientes', component : ConsultaClientesComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroClientesComponent,
+    ConsultaClientesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes) // registrando a configuração de rotas!
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cadastro-clientes/cadastro-clientes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-clientes',
+  templateUrl: './cadastro-clientes.component.html',
+  styleUrl: './cadastro-clientes.component.css'
+})
+export class CadastroClientesComponent {
+
+}
